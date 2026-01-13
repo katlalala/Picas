a401cd2c91e0fd124c57e329952ac09b6fa7f7d0
katlala <katrina.skudrite@sk.lvt.lv>
2026-01-13 10:49:25 +0200

    Picas klase iesākta.

--- src/Pica.java
+++ src/Pica.java
@@ -1,9 +1,93 @@
+class Pica {
+	
+		// ATRIBUTI
+		private double cena, lielums;
+		private String nosaukums, piedevas, merces;
+			
+			// KONSTRUKTORS
+		public Pica(double cena, double lielums, String nosaukums, String piedevas, String merces) {
+			this.nosaukums = nosaukums;
+			this.lielums = lielums;
+			this.piedevas = piedevas;
+			this.merces = merces;
+			this.cena = cena;
 
-public class Pica {
-
-	public static void main(String[] args) {
+		}
+			// METODES
+			
+			// case 0
+			public String getNosaukums() {
+				return nosaukums;
+		}
+			
+			// case 1
+			public double getLielums() {
+				return lielums;
+		}
+			
+			// case 2
+			public String getPiedevas() {
+				return piedevas;
+		}
+			
+			// case 3
+			public String getMerces() {
+				return merces;
+		}
+			
+			// case 4
+			public double getCena() {
+				return cena;
+		}
+			// nav pabeigts
+			
+			/*
 		
-
+			// case 5
+			public double NoteiktKusAtr() {
+				double celsM, laiksS;
+				Random rand = new Random();
+				celsM = rand.nextDouble()*(1000-1)+1;
+				laiksS = rand.nextDouble()*(60-10)+10;
+				return Math.round(celsM/laiksS);
+		}
+			// nav pabeigts
+			
+			
+			
+			// case 5
+			public double noteiktAtr() {
+				return atrumsMS;
+		}
+			
+			public String izvadit() {
+				return "Riteņa ražotājs: " + noteiktRaz() + "\nRiteņa cena: " + noteiktCenu() + " EUR"
+						+ "\nRiteņa lielums collās: " + noteiktRitenaD()
+						+ "\nIestatītais sēdekļa augstums: " + noteiktSedeklaPoz()
+						+ "\\nKustības ātrums: " + noteiktAtr() + "m/s";
+		}
+			
+			public void iestatitSedeklaPoz(int pozicija) {
+				sedeklaPoz = pozicija;
+		}
+			
+			public void mities(int minienuSk) {
+				atrumsMS += noteiktAtr() + (0.15 * minienuSk);
+		}
+			
+			public void bremzet(int berze) {
+				if(noteiktAtr() > 0) {
+					atrumsMS -= (noteiktAtr() * 0.55) * berze;
+					
+					if(atrumsMS < 0)
+						atrumsMS = 0;
+		}
 	}
-
-}
+				
+				@Override
+				public int compareTo(Velosipeds v) {
+					return this.noteiktCenu().compareTo(v.noteiktCenu());
+					
+				}
+				*/
+		}
